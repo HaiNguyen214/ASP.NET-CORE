c0abaf18c5b190b4385821fe94bbd5e1e8989e77
HaiNguyen214 <0306241276@caothang.edu.vn>
2026-09-24 14:40:46 +0700

    Update project

--- obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccc1e227f8848c5f4f7b28b9a47ddfc3e600d748")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
